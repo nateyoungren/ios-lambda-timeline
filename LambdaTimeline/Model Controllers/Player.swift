b0080f4434ba7e5a49f1e5bfb9118db515956969
nateyoungren <nathanaelyoungren@gmail.com>
2019-03-26 17:25:41 -0700

    Setup player

--- LambdaTimeline/Model Controllers/Player.swift
+++ LambdaTimeline/Model Controllers/Player.swift
@@ -8,27 +8,83 @@
 
 import AVFoundation
 
+protocol PlayerDelegate: AnyObject {
+    func playerDidChangeState(player: Player)
+}
+
 class Player: NSObject {
     
-    override init() {
-        super.init()
+    convenience init(url: URL) {
+        self.init()
+        self.url = url
+        audioPlayer = try? AVAudioPlayer(contentsOf: url)
+        audioPlayer?.delegate = self
     }
     
     func play() {
+        guard let audioPlayer = audioPlayer else { return }
+        audioPlayer.play()
+        notifyDelegate()
         
+        cancelTimer()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { (_) in
+            self.notifyDelegate()
+        })
     }
     
     func pause() {
-        
+        guard let audioPlayer = audioPlayer else { return }
+        audioPlayer.pause()
+        notifyDelegate()
     }
     
     func togglePlayPause() {
-        
+        if isPlaying {
+            pause()
+        } else {
+            play()
+        }
+    }
+    
+    func cancelTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    func notifyDelegate() {
+        delegate?.playerDidChangeState(player: self)
     }
     
     var isPlaying: Bool {
         return audioPlayer?.isPlaying ?? false
     }
     
+    var currentTime: TimeInterval {
+        return audioPlayer!.currentTime
+    }
+    
+    var duration: TimeInterval {
+        return audioPlayer!.duration
+    }
+    
+    var remainingTime: TimeInterval {
+        return duration - currentTime
+    }
+    
+    var url: URL?
+    weak var delegate: PlayerDelegate?
     private var audioPlayer: AVAudioPlayer?
+    var timer: Timer?
+}
+
+extension Player: AVAudioPlayerDelegate {
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        if let error = error {
+            print(error)
+        }
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        notifyDelegate()
+    }
 }
--- LambdaTimeline/View Controllers/CommentRecorderViewController.swift
+++ LambdaTimeline/View Controllers/CommentRecorderViewController.swift
@@ -12,13 +12,13 @@ class CommentRecorderViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         styleRecordButton()
         recordButton.setTitle("", for: .normal)
         
         let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         let name = ISO8601DateFormatter.string(from: Date(), timeZone: .current, formatOptions: .withInternetDateTime)
         let fileURL = directory.appendingPathComponent(name).appendingPathExtension("caf")
+        print(fileURL.path)
         self.url = fileURL
     }
     
@@ -29,13 +29,25 @@ class CommentRecorderViewController: UIViewController {
         recorder.toggleRecording()
         
         styleRecordButton()
+        player = Player(url: url)
+        player?.delegate = self
     }
     
     @IBAction func playPauseButtonWasTapped(_ sender: UIButton) {
-        
+        guard let player = player else { return }
+        player.togglePlayPause()
     }
     
     func styleRecordButton() {
+        
+        if player != nil {
+            updatePlayerViews()
+        } else {
+            audioSlider.alpha = 0
+            playButton.alpha = 0
+            elapsedTime.alpha = 0
+            remainingTime.alpha = 0
+        }
         
         recordButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
         recordButton.widthAnchor.constraint(equalTo: recordButton.heightAnchor).isActive = true
@@ -59,6 +71,27 @@ class CommentRecorderViewController: UIViewController {
         }
     }
     
+    func updatePlayerViews() {
+        guard let player = player else { return }
+        audioSlider.alpha = 1
+        playButton.alpha = 1
+        elapsedTime.alpha = 1
+        remainingTime.alpha = 1
+        
+        if player.isPlaying {
+            playButton.setTitle("Pause", for: .normal)
+        } else {
+            playButton.setTitle("Play", for: .normal)
+        }
+        
+        elapsedTime.text = timeFormatter.string(from: player.currentTime)
+        remainingTime.text = timeFormatter.string(from: player.remainingTime)
+        
+        audioSlider.maximumValue = Float(player.duration)
+        audioSlider.minimumValue = 0
+        audioSlider.value = Float(player.currentTime)
+    }
+    
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var elapsedTime: UILabel!
@@ -67,5 +100,21 @@ class CommentRecorderViewController: UIViewController {
     
     var url: URL?
     
-    var recorder = Recorder()
+    let recorder = Recorder()
+    var player: Player?
+    
+    private lazy var timeFormatter: DateComponentsFormatter = {
+        let f = DateComponentsFormatter()
+        f.unitsStyle = .positional
+        f.zeroFormattingBehavior = .pad
+        f.allowedUnits = [.minute, .second]
+        return f
+    }()
+}
+
+extension CommentRecorderViewController: PlayerDelegate {
+    
+    func playerDidChangeState(player: Player) {
+        updatePlayerViews()
+    }
 }
